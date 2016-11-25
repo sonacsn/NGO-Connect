bd8647a77b8b65846c6badbe6127d69824269091
Rahul Verma <verma.rah@Husky.neu.edu>
2016-11-25 18:53:43 -0500

    work mysql

--- public/server/app.js
+++ public/server/app.js
@@ -13,54 +13,56 @@ module.exports = function(app) {
 
         console.log('The solution is: ',rows);
     });
-
-
-   /*
-
-
-
-
-    var options = {
-    user:'root',
-    password:'rahul',
-    server:'localhost',
-    port:3306,
-    datatase:'project'
-    };
-
-    mysql.connect(options).then(function() {
-    console.log("connection completed");
-
-    }).catch(function(err) {
-    console.log("connection error"+err);
-    // ... connect error checks
-    })
-
-    "mssql://root:rahul@localhost:3306/project"
-
-   var mssql = require('mssql');
-    var options = {
-    user:'root',
-    password:'rahul',
-    server:'localhost',
-    port:3306,
-    datatase:'project'
-    };
-    mssql.connect(options);
-*/
-
-   /* var modeluser = require("./models/user.model.js")(mongoose, db);
-    require("./services/user.service.server.js")(app, modeluser,otherusermodel);
-
-    var modelforms = require("./models/form.model.js")(mongoose, db);
-    require("./services/form.service.server.js")(app, modelforms);
-
-    require("./services/field.service.server.js")(app, modelforms);*/
-
-
-
-
-
 };
+
+
+// HELP CODE
+
+
+/*
+
+
+
+
+ var options = {
+ user:'root',
+ password:'rahul',
+ server:'localhost',
+ port:3306,
+ datatase:'project'
+ };
+
+ mysql.connect(options).then(function() {
+ console.log("connection completed");
+
+ }).catch(function(err) {
+ console.log("connection error"+err);
+ // ... connect error checks
+ })
+
+ "mssql://root:rahul@localhost:3306/project"
+
+ var mssql = require('mssql');
+ var options = {
+ user:'root',
+ password:'rahul',
+ server:'localhost',
+ port:3306,
+ datatase:'project'
+ };
+ mssql.connect(options);
+ */
+
+/* var modeluser = require("./models/user.model.js")(mongoose, db);
+ require("./services/user.service.server.js")(app, modeluser,otherusermodel);
+
+ var modelforms = require("./models/form.model.js")(mongoose, db);
+ require("./services/form.service.server.js")(app, modelforms);
+
+ require("./services/field.service.server.js")(app, modelforms);*/
+
+
+
+
 
 
